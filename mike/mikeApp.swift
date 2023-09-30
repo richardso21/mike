0cdb4a288bbc653efc650e1ea6fbec2a8fa67498
Richard So <richardso2021@gmail.com>
2023-09-30 16:51:15 -0400

    Initial Commit

---
+++ mike/mikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mikeApp.swift
+//  mike
+//
+//  Created by Richard So on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct mikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
